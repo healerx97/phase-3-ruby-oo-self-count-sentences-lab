aa6055756dd46ea7ef26e06fb4fcd5e80c3b832c
Kenny Yoon <ghun.yoon@gmail.com>
2021-06-29 16:53:49 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,45 @@ require 'pry'
 class String
 
   def sentence?
+    if self[-1] == "."
+      true
+    else
+      false
+    end
     
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    count = 0
+    spec = "?.!"
+    arry = self.split("")
+    arry.each do |char|
+      asd = arry.find_index(char)
+      if (spec.include? char)
+        count += 1
+      end
+      if (spec.include? char) && (arry[asd+1])? (spec.include? arry[asd+1]): false
+        count -= 1
+      end
+    end
 
+    count
   end
+
 end
